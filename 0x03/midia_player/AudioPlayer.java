31bd2b2a6b6e5eeafbffd11f033c5491e5ee029f
Cesar A. Pires <cesarp@ciandt.com>
2022-07-22 13:45:07 -0300

    Projeto dia 53

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -1,6 +1,13 @@
 public class AudioPlayer implements MediaPlayer {
     @Override
     public void reproduzir(TipoMedia tipoMedia, String nome) {
-        System.out.print("Reproduzindo VLC: " + nome);
+        MediaPlayerAdapter mediaPlayerAdapter;
+
+        if (tipoMedia.equals(TipoMedia.MP3)) {
+            System.out.print("Reproduzindo MP3: " + nome);
+        } else {
+            mediaPlayerAdapter = new MediaPlayerAdapter(tipoMedia);
+            mediaPlayerAdapter.reproduzir(tipoMedia, nome);
+        }
     }
 }
