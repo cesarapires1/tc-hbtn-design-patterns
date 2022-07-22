3ac28a96f711e7d1d8ead80c779ffcadffbb932a
Cesar A. Pires <cesarp@ciandt.com>
2022-07-22 13:22:28 -0300

    Projeto dia 53

--- 0x03/naves/NaveEspacialDecorator.java
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -1,5 +1,5 @@
 public class NaveEspacialDecorator extends NaveEspacial{
-    private NaveEspacial naveEspacial;
+    private NaveEspacial naveDecorada;
 
     public NaveEspacialDecorator(NaveEspacial naveEspacial) {
         super(naveEspacial.getSaude(), naveEspacial.getAtaque());
@@ -7,11 +7,11 @@ public class NaveEspacialDecorator extends NaveEspacial{
     }
 
     public NaveEspacial getNaveEspacial() {
-        return naveEspacial;
+        return naveDecorada;
     }
 
     public void setNaveEspacial(NaveEspacial naveEspacial) {
-        this.naveEspacial = naveEspacial;
+        this.naveDecorada = naveEspacial;
     }
 
     @Override
